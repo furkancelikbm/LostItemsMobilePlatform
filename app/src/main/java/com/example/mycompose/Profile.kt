b7344098c406d037a883d9a000f6a72621d6c9f6
Furkan Celik <furkancelikbm@hotmail.com>
2024-09-14 18:57:26 +0300

    Profile.kt icindekiler silindi , mycompose.ProvileViewModel silindi

--- app/src/main/java/com/example/mycompose/Profile.kt
+++ app/src/main/java/com/example/mycompose/Profile.kt
@@ -21,86 +21,24 @@ import com.example.mycompose.widgets.ProfileImage
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
-fun Profile(navController: NavController, viewModel: ProfileViewModel = viewModel()) {
+fun Profile(navController: NavController) {
     val context = LocalContext.current
     // Display progress bar if profile is loading
-    TransparentCircularProgressBar(isLoading = viewModel.loading)
 
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(modifier = Modifier.padding(top = 25.dp))
-            ProfileImage(
-                imageUrl = viewModel.newPicUri.value ?: Uri.parse(viewModel.picUrl.value),
-                onImageChangeClick = { uri ->
-                    viewModel.newPicUri.value = uri
-                }
-            )
-            Spacer(modifier = Modifier.padding(vertical = 25.dp))
-            OutlinedTextField(
-                value = viewModel.editFirstName.value,
-                onValueChange = { viewModel.editFirstName.value = it },
-                label = { Text(text = "First name") },
-                readOnly = !viewModel.editMode.value
-            )
-            Spacer(modifier = Modifier.padding(vertical = 5.dp))
-            OutlinedTextField(
-                value = viewModel.editLastName.value,
-                onValueChange = { viewModel.editLastName.value = it },
-                label = { Text(text = "Last name") },
-                readOnly = !viewModel.editMode.value
-            )
-            Spacer(modifier = Modifier.padding(vertical = 25.dp))
-
-            // Show error message if any
-            if (viewModel.errorMessage.value.isNotEmpty()) {
-                Text(
-                    text = viewModel.errorMessage.value,
-                    color = MaterialTheme.colorScheme.error,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-            }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Button(
-                    onClick = {
-                        if (viewModel.editMode.value) {
-                            viewModel.saveProfileData(context, navController, "Home")
-                        }
-                        viewModel.editMode.value = !viewModel.editMode.value
-                    },
-                    modifier = Modifier.height(40.dp),
-                ) {
-                    Text(text = if (!viewModel.editMode.value) "Edit Profile" else "Save")
-                    Spacer(modifier = Modifier.padding(horizontal = 5.dp))
-                    Icon(imageVector = if (!viewModel.editMode.value) Icons.Default.Edit else Icons.Default.Save, contentDescription = null)
-                }
-                Spacer(modifier = Modifier.padding(horizontal = 5.dp))
-                Button(
-                    onClick = {
-                        FirebaseAuth.getInstance().signOut()
-                        navController.navigate("Login")
-                    },
-                    modifier = Modifier.height(40.dp),
-                ) {
-                    Text(text = "Log Out")
-                    Spacer(modifier = Modifier.padding(horizontal = 5.dp))
-                    Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
-                }
-            }
-        }
+    Column(
+        modifier = Modifier
+            .fillMaxSize(), // Fill the entire screen
+        verticalArrangement = Arrangement.Center, // Center vertically
+        horizontalAlignment = Alignment.CenterHorizontally // Center horizontally
+    ) {
+        Text(text = "napiyon lan")
     }
-
+}
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewProfileScreen(navController: NavController = rememberNavController()) {
     Profile(navController = navController)
 }
+
 
