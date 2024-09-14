0895be8be38b8de2e46f55ce685d7c36368cfdac
Furkan Celik <furkancelikbm@hotmail.com>
2024-09-14 04:20:33 +0300

    BottomNavBar gecerli sayfada olup selectedIcon duzeltildi

--- app/src/main/java/com/example/mycompose/NavigationBar/BottomNavigationBar.kt
+++ app/src/main/java/com/example/mycompose/NavigationBar/BottomNavigationBar.kt
@@ -12,34 +12,37 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BottomNavigationBar(navController: NavController,viewModel: NavigationViewModel) {
+fun BottomNavigationBar(navController: NavController, viewModel: NavigationViewModel) {
     val items = listOf(
         NavItem("Home", Icons.Filled.Home, Icons.Outlined.Home),
         NavItem("Profile", Icons.Filled.Person, Icons.Outlined.Person)
     )
 
-    val selectedIndex by viewModel.selectedItemIndex
+    // Geçerli sayfa yolunu al
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route ?: ""
 
     NavigationBar {
         items.forEachIndexed { index, item ->
             NavigationBarItem(
                 icon = {
                     Icon(
-                        imageVector = if (selectedIndex == index) item.selectedIcon else item.unSelectedIcon,
+                        imageVector = if (currentRoute == item.title) item.selectedIcon else item.unSelectedIcon,
                         contentDescription = item.title
                     )
                 },
                 label = { Text(text = item.title) },
-                selected = selectedIndex == index,
+                selected = currentRoute == item.title,
                 onClick = {
                     viewModel.onNavigationItemSelected(index)
-                    // Perform navigation action
                     navController.navigate(item.title) {
-                        // Clear back stack to prevent multiple instances of the same destination
+                        // Aynı hedefe birden fazla giriş yapılmasını önlemek için back stack temizleme
                         popUpTo(navController.graph.startDestinationId) {
                             saveState = true
                         }
@@ -51,3 +54,5 @@ fun BottomNavigationBar(navController: NavController,viewModel: NavigationViewMo
         }
     }
 }
+
+
