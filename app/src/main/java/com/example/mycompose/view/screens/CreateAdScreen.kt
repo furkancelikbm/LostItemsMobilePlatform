4a8c938baa9599cb7e8a85f7298ff2bfd8ab0c75
Furkan Çelik <furkancelikbm@hotmail.com>
2025-01-19 19:11:27 +0300

    scrollable for createAdScreen

--- app/src/main/java/com/example/mycompose/view/screens/CreateAdScreen.kt
+++ app/src/main/java/com/example/mycompose/view/screens/CreateAdScreen.kt
@@ -15,8 +15,10 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
@@ -52,26 +54,21 @@ fun CreateAdScreen(
     val context = LocalContext.current
     val focusManager = LocalFocusManager.current
 
-    // Inject AdPredictionViewModel with Hilt
     val predictionViewModel: AdPredictionViewModel = hiltViewModel()
 
-    // Gallery launcher for image selection
     val galleryLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.OpenDocument()
     ) { uri: Uri? -> uri?.let { viewModel.addImage(it) } }
 
-    // Selected category and most frequent prediction label
     val selectedCategory = viewModel.selectedCategory.value
     val mostFrequentLabel = predictionViewModel.predictionResult.value
 
-    // Observe saved category
     val backStackEntry = navController.currentBackStackEntry
     val savedStateHandle = backStackEntry?.savedStateHandle
     savedStateHandle?.getLiveData<String>("selectedCategory")?.observeAsState()?.value?.let { category ->
         category?.let { viewModel.selectedCategory.value = it }
     }
 
-    // Trigger prediction for each selected image
     val selectedImages = viewModel.selectedImages
     if (selectedImages.isNotEmpty()) {
         selectedImages.forEach { (uri, _) ->
@@ -90,15 +87,13 @@ fun CreateAdScreen(
     val latitude = navController.currentBackStackEntry?.savedStateHandle?.get<Double>("latitude")
     val longitude = navController.currentBackStackEntry?.savedStateHandle?.get<Double>("longitude")
 
-    // Veriler geldiğinde, boş değilse atama yap
     if (latitude != null && longitude != null) {
         viewModel.locationValidation.value = location.value.text
         viewModel.locationInputFieldViewModel.updateLocationData(longitude, latitude, location.value)
     } else {
-        Log.d("Merhaba", "Latitude veya Longitude verisi gelmedi")
+        Log.d("CreateAdScreen", "Latitude or Longitude not provided")
     }
 
-    // UI layout
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -110,10 +105,10 @@ fun CreateAdScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp),
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()), // Scrollable column
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // Header
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.CenterVertically
@@ -127,7 +122,6 @@ fun CreateAdScreen(
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Title and Description Input Fields
             AdTextField(
                 value = viewModel.adModel.value.title,
                 onValueChange = { viewModel.adModel.value = viewModel.adModel.value.copy(title = it) },
@@ -148,11 +142,9 @@ fun CreateAdScreen(
                 value = viewModel.locationInputFieldViewModel.pickUp,
                 onValueChange = { newValue ->
                     viewModel.locationInputFieldViewModel.onPickUpValueChanged(newValue)
-                    // Check if value is cleared
                     if (newValue.text.isEmpty()) {
-                        // Clear location value and set the pickup location to viewModel
                         viewModel.locationInputFieldViewModel.clearPickUpLocation()
-                        location.value = TextFieldValue("")  // Clear the TextField value
+                        location.value = TextFieldValue("")
                     }
                 },
                 placeholder = "Pickup Location",
@@ -160,36 +152,27 @@ fun CreateAdScreen(
                 onLocationClick = { place ->
                     viewModel.locationInputFieldViewModel.onPlaceClick(place.name)
                     viewModel.locationValidation.value = place.id
-                    Log.d("CreateAdScreen", "Selected Place ID: ${place.id}")
                 },
                 checkAndFirstPlace = {
                     viewModel.locationInputFieldViewModel.checkAndSelectFirstPlace()
                     viewModel.locationValidation.value = viewModel.locationInputFieldViewModel.unSelectedLocationId
                 },
                 onLocationButtonClick = {
-                    Log.d("CreateAdScreen", "Location button clicked")
-                    navController.navigate("mapScreen") // Navigate to the MapScreen
+                    navController.navigate("mapScreen")
                 },
                 onRemoveClick = {
-                    viewModel.locationInputFieldViewModel.clearPickUpLocation() // Clear location in ViewModel
-                    location.value = TextFieldValue("")  // Clear the TextField value
-                    Log.d("CreateAdScreen", "Pickup location cleared")
+                    viewModel.locationInputFieldViewModel.clearPickUpLocation()
+                    location.value = TextFieldValue("")
                 }
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Category Selector
             OutlinedTextField(
                 value = if (selectedCategory.isNotEmpty()) selectedCategory else "Choose a Category",
                 onValueChange = {},
                 readOnly = true,
-                label = {
-                    Text(
-                        "Category",
-                        style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 17.sp)
-                    )
-                },
+                label = { Text("Category", style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 17.sp)) },
                 modifier = Modifier
                     .fillMaxWidth()
                     .clickable { navController.navigate("ChooseCategoryScreen") },
@@ -201,17 +184,14 @@ fun CreateAdScreen(
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Prediction Label
             Text(text = "Prediction: $mostFrequentLabel", style = MaterialTheme.typography.bodyLarge)
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Select Photos Button
             AdButton(onClick = { galleryLauncher.launch(arrayOf("image/*")) }, text = "Select Photos")
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Selected Images Gallery
             LazyRow {
                 items(viewModel.selectedImages) { (uri, uniqueId) ->
                     Box(
@@ -248,7 +228,6 @@ fun CreateAdScreen(
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Error Message Display
             if (viewModel.showError.value) {
                 Text(
                     text = viewModel.errorMessage.value,
@@ -258,7 +237,6 @@ fun CreateAdScreen(
                 Spacer(modifier = Modifier.height(8.dp))
             }
 
-            // Submit Button
             AdButton(
                 onClick = {
                     focusManager.clearFocus()
@@ -271,7 +249,6 @@ fun CreateAdScreen(
             )
         }
 
-        // Loading Progress Bar
         TransparentCircularProgressBar(isLoading = viewModel.isLoading.value)
     }
 }
